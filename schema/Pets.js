99947ee2b46af7db61afd0eed07fcfc51f9f687f
pisit.t <pisit.t@t2pco.com>
2022-12-12 15:41:10 +0000

    "Initial commit"

---
+++ schema/Pets.js
@@ -0,0 +1,52 @@
+cube(`Pets`, {
+  sql: `SELECT * FROM transactionscript.pets`,
+  
+  preAggregations: {
+    // Pre-Aggregations definitions go here
+    // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started
+  },
+  
+  joins: {
+    
+  },
+  
+  measures: {
+    count: {
+      type: `count`,
+      drillMembers: [id, name]
+    }
+  },
+  
+  dimensions: {
+    age: {
+      sql: `age`,
+      type: `number`
+    },
+    
+    isSold: {
+      sql: `is_sold`,
+      type: `number`
+    },
+    
+    id: {
+      sql: `id`,
+      type: `string`,
+      primaryKey: true
+    },
+    
+    name: {
+      sql: `name`,
+      type: `string`
+    },
+    
+    addedAt: {
+      sql: `added_at`,
+      type: `time`
+    },
+    
+    soldAt: {
+      sql: `sold_at`,
+      type: `time`
+    }
+  }
+});
